f321ea4ac2dcb33f73290681bd09aec34d6d8e35
Veronika Dzúriková <xvera6@gmail.com>
2021-10-10 16:13:15 +0200

    🎉 Init xamarin project

---
+++ src/Filaaide.Core/ViewModels/Home/HomeViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Filaaide.Core.ViewModels._Base;
+
+namespace Filaaide.Core.ViewModels.Home
+{
+    public class HomeViewModel : BaseViewModel
+    {
+    }
+}
